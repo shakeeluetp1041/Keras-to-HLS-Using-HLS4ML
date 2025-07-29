416f4e1b02c2c8771aee3aa9eea0422338ef8456
shakeeluetp1041 <shakeeluetp1041@gmail.com>
2025-07-29 11:30:26 +0200

    first hls4ml model conversion without build

---
+++ model_1/hls4ml_prj/firmware/weights/b11.h
@@ -0,0 +1,16 @@
+//Numpy array shape [5]
+//Min -0.054795973003
+//Max 0.070124007761
+//Number of zeros 0
+
+#ifndef B11_H_
+#define B11_H_
+
+#ifndef __SYNTHESIS__
+model_default_t b11[5];
+#else
+model_default_t b11[5] = {-0.0547959730, -0.0093461536, -0.0151799247, 0.0701240078, 0.0316001549};
+
+#endif
+
+#endif
